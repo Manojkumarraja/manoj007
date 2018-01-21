8a77b0972273850322eb6bd4d3d9b2ea9d2f1d47
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-01-21 15:06:59 +0530

    Rename whether given number is positive or negative or zero.c to posneg.c

---
+++ posneg.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter the number");
+scanf("%d",&a);
+if(a<0)
+{
+ printf("\n The given number is negative");
+ }
+ else
+ if(a==0)
+ {
+ printf("\n The given number is zero");
+ }
+ else
+ {
+ printf("\n The given number is positive");
+ }
+ }
