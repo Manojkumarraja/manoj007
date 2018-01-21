85f27dbb677f2963064381ae5758e3d5aa2f5991
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-01-21 16:27:41 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -4,7 +4,7 @@ void main()
 char a[6]={'a','e','i','o','u'};
 char b;
 printf("Enter the alphabet");
-scanf("%c",b);
+scanf("%c",&b);
 if(a[0]==b||a[1]==b||a[2]==b||a[3]==b||a[4]==b)
 {
 printf("\n The given value is vowel");
