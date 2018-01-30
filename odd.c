ddfb5d37daea39619788febff709e65f4a6f7ad0
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-01-30 16:01:22 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter the two intervals");
+    scanf("%d%d",&a,&b);
+    for (c=a;c<=b;++c)
+    {
+        if(c%2!=0)
+    {
+        printf("\n%d",c);
+    }
+        else
+        {
+         int d;   
+        }
+    }
+    }
