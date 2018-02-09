e1010b35e0f9c89d1bab60d7ed1f2ce379e50d5d
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-02-09 21:28:55 +0530

    Create matdi2.c

---
+++ matdi2.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+    int n,a[10][10],p1,p2,res;
+    int i,j;
+    printf("Enter the size of the matrix");
+    scanf("%d",&n);
+    printf("\nEnter the array elements");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    p1=1;
+    i=0;
+    for(j=0;j<n;j++)
+    {
+        p1*=a[i][j];
+        i++;
+    }
+    i=0;
+    p2=1;
+    res=0;
+    for(j=n-1;j>=0;j--)
+    {
+        p2*=a[i][j];
+        i++;
+    }
+    res=p1+p2;
+    printf("\n%d",res);
+}
+
