d4a01cb01d437233fe477523e3b67dbeefdf9bf1
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-02-09 20:38:29 +0530

    Create triarea.c

---
+++ triarea.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int base,height,area;
+    printf("Enter the base of triangle");
+    scanf("%d",&base);
+    printf("\nEnter the height of the triangle");
+    scanf("%d",&height);
+    area=(0.5*base*height);
+    printf("\nArea of the triangle is %d",area);
+}
