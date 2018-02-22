756e451228c2e3aa49c8f1a36745b9c99efb0c4e
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-02-22 21:16:03 +0530

    Create concate.c

---
+++ concate.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    char a[10],b[10];
+    printf("Enter the string 1");
+    scanf("%s",&a);
+    printf("\nEnter the string 2");
+    scanf("%s",&b);
+    printf("%s%s",a,b);
+}
