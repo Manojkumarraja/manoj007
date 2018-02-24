1fd9233b31d72e7e4ba6009fe4e5961edf4b7dd1
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-02-24 23:05:20 +0530

    Create 7mult.c

---
+++ 7mult.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter a number");
+    scanf("%d",&a);
+    if(a%7==0)
+    {
+        printf("\nYES");
+    }
+    else
+    {
+        printf("\nNO");
+    }
+}
