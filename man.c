99501d041cb6d7c59d9685253a351537a7624fce
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-02-24 22:43:27 +0530

    Update man.c

--- man.c
+++ man.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 void main()
 {
-    char a[10]="manoj";
+    char a[10];
     int n,i;
+    printf("Enter a string");
+    scanf("%s",a);
     printf("Enter a number\n");
     scanf("%d",&n);
     for(i=0;i<n;i++)
