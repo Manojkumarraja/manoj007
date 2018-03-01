a1f6179bceeb83c8df71beb14cd9e8bc64342bdf
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-03-01 15:49:19 +0530

    Create str*.c

---
+++ str*.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char a[10];
+    int i,n;
+    printf("Enter a string");
+    scanf("%s",&a);
+    n=0;
+    for(i=0;a[i]!='\0';i++)
+    {
+        n++;
+    }
+    a[n/2]='*';
+    printf("%s",a);
+}
