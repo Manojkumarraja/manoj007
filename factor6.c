d30a5db911c745ca42279e9e9a35e8bc27cf759a
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-03-01 15:55:55 +0530

    Create factor6.c

---
+++ factor6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int n,i;
+    printf("Enter a number");
+    scanf("%d",&n);
+    printf("\nFactors are\n");
+    for(i=1;i<=6;i++)
+    {
+        printf("\t%d",i);
+    }
+}
