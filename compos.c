a23221e6a1785916177b1938dafec6be765d42de
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-03-02 14:12:46 +0530

    Update compos.c

--- compos.c
+++ compos.c
@@ -4,19 +4,19 @@ void main()
     int a,i,count=0;
     printf("Enter a number");
     scanf("%d",&a);
-    for(i=2;i<=a/2;i++)
+    for(i=2;i<=a;i++)
     {
         if(a%i==0)
         {
             count++;
         }
     }
-    if(count==0)
+    if(count==1)
     {
-        printf("\nYES");
+        printf("\nNO");
     }
     else
     {
-        printf("\nNO");
+        printf("\nYES");
     }
 }
