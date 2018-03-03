bdf0a0a8b98320bba6e5ee716bebc29b32fe6924
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-03-03 13:24:38 +0530

    Create near10.c

---
+++ near10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a,i;
+    printf("Enter a number");
+    scanf("%d",&a);
+    for(i=10;a<=a;i+=10)
+    {
+        if(a<i)
+        {
+            printf("\n%d",i);
+            break;
+        }
+      
+    }
+}
