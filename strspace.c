0fd188ac9f3df60df5a2e3a91027b5a327f6d7f0
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-03-03 13:46:22 +0530

    Update strspace.c

--- strspace.c
+++ strspace.c
@@ -1,22 +1,17 @@
-
 #include<stdio.h>
-#include<string.h>
 void main()
 {
-char a[1000];
-int b = 0, c= 0, d = 0;
-puts("Enter any string\n");
-gets(a);
-for(b= 0; a[b] !='\0'; b++)
-{
-c = c + 1;
-}
-for(b = 0; b <= c-1; b++)
-{
-if(a[b] == ' ')
-{
-d = d + 1;
-}
-}
-printf("The number of space in the line are %d", d);
+    char a[20];
+    int i,num=0;
+    printf("Enter a string");
+    scanf("%[^\n]",&a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==' ')
+        {
+            num++;
+        }
+    }
+    printf("\nString space is %d",num);
+
 }
