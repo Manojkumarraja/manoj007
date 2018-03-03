a386ddaffd2c81cba55e7a23774d21cd02004e09
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-03-03 14:24:03 +0530

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],n,n1,j,i;
+    printf("Enter the array size");
+    scanf("%d",&n);
+    printf("\nEnter the array values");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+    {
+        if(a[i]>a[j])
+        {
+            n1=a[i];
+            a[i]=a[j];
+            a[j]=n1;
+        }
+    }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("\t%d",a[i]);
+    }
+    printf("%d",a[(n-1)/2]);
+}
