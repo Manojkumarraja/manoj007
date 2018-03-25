f52698a848393b7454edf09531a89b0bc296e213
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-03-25 09:19:19 +0530

    Update 103.c

--- 103.c
+++ 103.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 void main()
 {
-    char a[20]="i love c++";
+    char a[20];
     int i;
+    printf("Enter the string");
+    scanf("%[^\n]",&a);
     for(i=0;a[i]!='\0';i++)
     {
         if(a[i]!=' ')
