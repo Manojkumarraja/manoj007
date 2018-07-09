ec686e1b57f61b4825b8afb3765a8a9a239116f7
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-07-09 22:12:08 +0530

    Update pro3.c

--- pro3.c
+++ pro3.c
@@ -3,7 +3,7 @@ int main()
 {
     int i,num=0;
     char a[10],b[10];
-    printf("Enter the two string");
+    //printf("Enter the two string");
     scanf("%s%s",a,b);
 for(i=0;((a[i]!='\0')||(b[i]!='\0'));i++)
 {
