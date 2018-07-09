0d82bba9afddb7cc1c445c0b4be998191d05d225
Manojkumarraja <35652353+Manojkumarraja@users.noreply.github.com>
2018-07-09 18:33:36 +0530

    Update PRO1.C

--- PRO1.C
+++ PRO1.C
@@ -2,7 +2,7 @@
 void main()
 {
     int i,j,n,num=0,n1=0;
-    char a[10],b[10],c[10],d[10];
+    char a[10],b[10],c[10],d[10],e[10];
     printf("Enter the number of string");
     scanf("%d",&n);
     switch(n)
@@ -70,7 +70,26 @@ void main()
                 printf("%c",a[i]);
             }
         }
-
-}
+        case 5:
+            {
+                printf("\nEnter the five string");
+                scanf("%s%s%s%s%s",a,b,c,d,e);
+                for(i=0;((a[i]!='\0')||(b[i]!='\0')||(c[i]!='\0')||(d[i]!='\0')||(e[i]!='\0'))
+                    {
+                        n1++;
+                    }
+                    for(i=0;i<n1;i++)
+                    {
+                        if((a[i]==b[i])&&(b[i]==c[i])&&(c[i]==d[i])&&(d[i]==e[i]))
+                        {
+                            num++;
+                        }
+                    }
+                    for(i=0;i<num;i++)
+                    {
+                        printf("%c",a[i]);
+                    }
+                    }
+                    }
     
 }
